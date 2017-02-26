32474842030325947aa44d2c3fe85c4e9dcb9cac
evemsilva <everton0189@gmail.com>
2017-02-26 16:44:28 -0300

    Ajuste para deploy no travis

--- public/js/controllers/ContatosController.js
+++ public/js/controllers/ContatosController.js
@@ -1,37 +1,33 @@
-angular.module('contatooh').controller('ContatosController', 
-  function(Contato, $scope) { 
-    $scope.contatos = [];
+angular.module('contatooh').controller('ContatosController', function(Contato, $scope) { 
+  $scope.contatos = [];
 
-    $scope.filtro = '';
+  $scope.filtro = '';
 
-    $scope.mensagem = {texto: ''};
+  $scope.mensagem = {texto: ''};
   
-    function buscaContatos() {
-      Contato.query(
-        function(contatos) {
-          $scope.contatos = contatos;
-          $scope.mensagem = {};
-        },
-        function(erro) {
-          console.log(erro);
-          $scope.mensagem = {
-            texto: 'Não foi possível obter a lista'
-          };
-        }
-      ); 
-    }
-    
-    buscaContatos();
+  function buscaContatos() {
+    Contato.query(
+      function(contatos) {
+        $scope.contatos = contatos;
+        $scope.mensagem = {};
+      },
+      function(erro) {
+        console.log(erro);
+        $scope.mensagem = {texto: 'Não foi possível obter a lista'};
+      }
+    ); 
+  }
+  
+  buscaContatos();
 
-    $scope.remove = function(contato) {
-      Contato.delete({id: contato._id}, 
-        buscaContatos, 
-        function(erro) {
-          $scope.mensagem = {
-            texto: 'Não foi possível remover o contato'
-          };
-          console.log(erro);
-        }
-      );
-    }; 
+  $scope.remove = function(contato) {
+    Contato.delete({id: contato._id}, 
+      buscaContatos, 
+      function(erro) {
+        $scope.mensagem = {texto: 'Não foi possível remover o contato'};
+        console.log(erro);
+      }
+    );
+  }; 
+
 });
